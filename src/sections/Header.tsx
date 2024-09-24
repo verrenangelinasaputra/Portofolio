49f4900abd74cbbe80bd433ded420b9c906e6afc
VerrenAngelina <Verren62@gmail.com>
2024-09-24 23:12:43 +0700

    hero sec commit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center relative top-3">
+    <div className="flex justify-center items-center relative top-3 w-full z-10">
       {/* p-0.5 means padding */}
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         {/* className calls the global variables instead of local that isn't efficient to use */}
