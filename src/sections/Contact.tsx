bd1072c92e20940171352eb953e77a7387a07602
VerrenAngelina <verren.angelinas@gmail.com>
2024-10-21 09:58:36 +0700

    Update Contact-4

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,8 +3,8 @@ import grainImage from '@/assets/images/grain.jpg';
 
 export const ContactSection = () => {
   return (
-    <div className="py-16 pt-14">
-      <div className="container">
+    <div className="py-16 pt-14 lg:py-24 lg:pt-20">
+      <div className="container ">
         <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
           <div className="absolute inset-0 opacity-10 -z-10"
             style={{
