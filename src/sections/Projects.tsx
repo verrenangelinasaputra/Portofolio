47616a2cd68af9ca40b323711e6279ba1e7ee46e
VerrenAngelina <verren.angelinas@gmail.com>
2024-10-22 10:50:13 +0700

    Update Project-4

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -55,7 +55,7 @@ export const ProjectsSection = () => {
       <p className="text-center sm:mt-8 md:text-lg lg:mt-8 text-white/60 mt4 max-w-3xl mx-auto">Discover my portfolio of projects in artificial intelligence, data analytics, and web development. Each project highlights my skills and dedication to using technology to address practical challenges. Explore case studies that demonstrate my innovative solutions in various technical fields.</p>
       <div className="mt-10 md:mt-20 flex flex-col gap-20">
         {portfolioProjects.map(project => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none">
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky top-16 after:pointer-events-none">
 
             <div className="absolute inset-0 -z-10 opacity-5" style={{
               backgroundImage: `url(${GrainImage.src})`
