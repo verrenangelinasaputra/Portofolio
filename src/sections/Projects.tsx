60954aac98ae68346a2a06cefba6dbbde92cd1b0
VerrenAngelina <verren.angelinas@gmail.com>
2024-10-24 23:50:49 +0700

    New Update-2
    Project benerin error

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -76,10 +76,10 @@ export const ProjectsSection = () => {
             <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
             <ul className="flex flex-col gap-4 mt-4 md:mt-5">
               {project.results.map(result => (
-                <li className="flex gap-2 text-sm md:text-base text-white/50">
-                  <CheckCircleIcon className="size-5 md:size-6"/>
-                  <span>{result.title}</span>
-                </li>
+              <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
+                <CheckCircleIcon className="size-5 md:size-6"/>
+                <span>{result.title}</span>
+              </li>
               ))}
             </ul>
             <Link href={project.link}>
