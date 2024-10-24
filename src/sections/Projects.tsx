163c254288d9f4e336ca54c8db2d1c3e5fe2b22b
VerrenAngelina <verren.angelinas@gmail.com>
2024-10-25 00:12:10 +0700

    Update Project-7
    Key added

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,7 +6,6 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import GrainImage from '@/assets/images/grain.jpg';
 import Link from 'next/link';
-// import { AIProjectSection } from "@/sections/AIProject";
 
 const portfolioProjects = [
   {
@@ -76,10 +75,10 @@ export const ProjectsSection = () => {
             <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
             <ul className="flex flex-col gap-4 mt-4 md:mt-5">
               {project.results.map(result => (
-              <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
-                <CheckCircleIcon className="size-5 md:size-6"/>
-                <span>{result.title}</span>
-              </li>
+                <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
+                  <CheckCircleIcon className="size-5 md:size-6"/>
+                  <span>{result.title}</span>
+                </li>
               ))}
             </ul>
 
@@ -91,7 +90,7 @@ export const ProjectsSection = () => {
             </Link>
             </div>
             <div className="relative">
-            <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
             </div>
             </div>
           </div>
