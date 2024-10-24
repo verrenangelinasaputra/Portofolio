b4a3fec9f4edaa862dfeddabec47f2a792e69d63
VerrenAngelina <verren.angelinas@gmail.com>
2024-10-25 00:01:10 +0700

    Update Project-6

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -82,6 +82,7 @@ export const ProjectsSection = () => {
               </li>
               ))}
             </ul>
+
             <Link href={project.link}>
               <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                 <span>View More</span>
